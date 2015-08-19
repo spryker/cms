06fd6032c3d4335a222f19a4a92f502fcdc67292
Ehsan <ehsan.zanjani@spryker.com>
2015-08-19 15:34:45 +0000

    KSP-544 Cms client added

---
+++ src/SprykerFeature/Client/Cms/Service/KeyBuilder/CmsBlockKeyBuilder.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * (c) Spryker Systems GmbH copyright protected
+ */
+
+namespace SprykerFeature\Client\Cms\Service\KeyBuilder;
+
+use SprykerFeature\Shared\CmsExporter\Code\KeyBuilder\SharedBlockResourceKeyBuilder;
+
+class CmsBlockKeyBuilder extends SharedBlockResourceKeyBuilder
+{
+}
